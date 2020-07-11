e83b6b0a2de43ddbadf0979cb5fc8d44fd0b8511
Abdullah1428 <abdullahcse1428@gmail.com>
2020-07-11 11:27:11 +0500

    added openCV configuration

---
+++ AES.hpp
@@ -0,0 +1,11 @@
+// here all main libraries will be added that will be used in the project
+#include <opencv2/opencv.hpp>
+#include <stdint.h>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+using namespace cv;
+
+Mat subByte(Mat );
+void block_encryption();
